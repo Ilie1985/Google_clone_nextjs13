64ccfa3d6642d74bc273cad02281c25b17dfed72
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-13 14:48:43 +0000

    created the form section in homeSearch component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
 import React, { Fragment } from "react";
 import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "@/components/HomeSearch";
 
 const Home = () => {
   return (
     <Fragment>
+      {/* HEADER */}
       <HomeHeader />
+
+      {/* BODY */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+        />
+
+
+        <HomeSearch/>
+      </div>
     </Fragment>
   );
 };
