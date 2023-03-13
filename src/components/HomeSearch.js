aea93b488a1306c4e8f4a2a3c89b7f643d55b4ee
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-13 15:05:23 +0000

    buttons UI in HomeSearch component

--- src/components/HomeSearch.js
+++ src/components/HomeSearch.js
@@ -7,9 +7,14 @@ const HomeSearch = () => {
     <Fragment>
       <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200  px-5 py-3 rounded-full hover:shadow-md transition-shadow focus-within:shadow-md sm:max-w-xl lg:max-w-2xl">
         <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
-        <input type="text" className="flex-grow focus:outline-none"/>
-        <BsFillMicFill className="text-lg"/>
+        <input type="text" className="flex-grow focus:outline-none" />
+        <BsFillMicFill className="text-lg" />
       </form>
+
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
+        <button className="btn">Google Search</button>
+        <button className="btn">I Am Feeling Lucky</button>
+      </div>
     </Fragment>
   );
 };
