a8c9d5e36f2d8d4c9d824d00b4241aacba844f00
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-20 16:14:14 +0000

    fetch data  with google search Api show the title

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
-const WebSearchPage = () => {
+const WebSearchPage = async ({ searchParams }) => {
+  const res = await fetch(
+    ` https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await res.json();
+
+  const results = data.items;
   return (
-    <div>WebSearchPage</div>
-  )
-}
+    <Fragment>
+      {results &&
+        results.map((result) => {
+          return <h1 key={result.id}>{result.title}</h1>;
+        })}
+    </Fragment>
+  );
+};
 
-export default WebSearchPage
+export default WebSearchPage;
