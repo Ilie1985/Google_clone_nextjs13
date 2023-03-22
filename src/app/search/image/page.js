7da9665bcaf534be111d161e693a3bd2db4dabe2
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-22 10:32:28 +0000

    add imageResults

--- src/app/search/image/page.js
+++ src/app/search/image/page.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { Fragment } from "react";
+import Link from "next/link";
+import ImageSearchResults from "@/components/ImageSearchResults";
 
-const ImageSearchPage = () => {
-  return (
-    <div>ImageSearchPage</div>
-  )
-}
+const ImageSearchPage = async ({ searchParams }) => {
+  await new Promise((resolve) => {
+    return setTimeout(resolve, 10000);
+  });
+  const res = await fetch(
+    ` https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
 
-export default ImageSearchPage
+  const data = await res.json();
+
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10  ">
+        <h1 className="text-3xl mb-4">No results for this search</h1>
+        <p className="text-lg">
+          Try search something else or go back to home page
+        </p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </div>
+    );
+  }
+  return <Fragment>{results && <ImageSearchResults data={data} />}</Fragment>;
+};
+
+export default ImageSearchPage;
