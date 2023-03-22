0ef5c5e2b57b9d9b80f957a64216543290d43941
Ilie1985 <ilie.marian32@yahoo.com>
2023-03-22 10:02:32 +0000

    create searchResult component

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,32 +1,33 @@
 import React, { Fragment } from "react";
 import Link from "next/link";
+import WebSearchResults from "@/components/WebSearchResults";
 
 const WebSearchPage = async ({ searchParams }) => {
+  await new Promise((resolve) => {
+    return setTimeout(resolve, 10000);
+  });
   const res = await fetch(
     ` https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
   const data = await res.json();
- 
+
   const results = data.items;
 
   if (!results) {
-    return <div className="flex flex-col justify-center items-center pt-10  ">
-       <h1 className="text-3xl mb-4">No results for this search</h1>
-       <p className="text-lg">Try search something else or go back to home page</p>
-       <Link href="/" className="text-blue-500">
-       Home
-       </Link>
-    </div>
+    return (
+      <div className="flex flex-col justify-center items-center pt-10  ">
+        <h1 className="text-3xl mb-4">No results for this search</h1>
+        <p className="text-lg">
+          Try search something else or go back to home page
+        </p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </div>
+    );
   }
-  return (
-    <Fragment>
-      {results &&
-        results.map((result) => {
-          return <h1 key={result.id}>{result.title}</h1>;
-        })}
-    </Fragment>
-  );
+  return <Fragment>{results && <WebSearchResults data={data} />}</Fragment>;
 };
 
 export default WebSearchPage;
